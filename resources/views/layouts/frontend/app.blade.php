3766c05bde4d0e95af8e498834c52fc201edee9c
HexaTeam <arvenaloverz@gmail.com>
2019-12-01 11:04:15 +0700

    frontend show post

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -33,6 +33,8 @@
 <script src="{{ asset('assets/frontend/js/jquery-3.1.1.min.js') }}"></script>
 <script src="{{ asset('assets/frontend/js/tether.min.js') }}"></script>
 <script src="{{ asset('assets/frontend/js/bootstrap.js') }}"></script>
+<script src="{{ asset('assets/frontend/js/swiper.js') }}"></script>
+<script src="{{ asset('assets/frontend/js/scripts.js') }}"></script>
 {{--Toastr--}}
 <script src="{{asset('assets/toastr.min.js')}}"></script>
 {!! Toastr::message() !!}
